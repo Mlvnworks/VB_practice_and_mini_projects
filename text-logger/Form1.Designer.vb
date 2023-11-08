ef8da1ed3a954a0bdc23c67da81f5f1785bf4e51
Melvin Agustin <agustinarsenio122@gmail.com>
2023-11-08 08:35:53 +0800

    add text logger project

---
+++ text-logger/Form1.Designer.vb
@@ -0,0 +1,87 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        OutputBox = New TextBox()
+        newWordInput = New TextBox()
+        addBtn = New Button()
+        Button1 = New Button()
+        SuspendLayout()
+        ' 
+        ' OutputBox
+        ' 
+        OutputBox.Location = New Point(12, 62)
+        OutputBox.Multiline = True
+        OutputBox.Name = "OutputBox"
+        OutputBox.ReadOnly = True
+        OutputBox.Size = New Size(776, 376)
+        OutputBox.TabIndex = 0
+        ' 
+        ' newWordInput
+        ' 
+        newWordInput.Font = New Font("Arial Narrow", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        newWordInput.Location = New Point(12, 21)
+        newWordInput.Name = "newWordInput"
+        newWordInput.Size = New Size(390, 26)
+        newWordInput.TabIndex = 1
+        ' 
+        ' addBtn
+        ' 
+        addBtn.Location = New Point(408, 21)
+        addBtn.Name = "addBtn"
+        addBtn.Size = New Size(75, 26)
+        addBtn.TabIndex = 2
+        addBtn.Text = "Add"
+        addBtn.UseVisualStyleBackColor = True
+        ' 
+        ' Button1
+        ' 
+        Button1.BackColor = Color.LightCoral
+        Button1.ForeColor = SystemColors.ButtonHighlight
+        Button1.Location = New Point(503, 21)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(75, 26)
+        Button1.TabIndex = 3
+        Button1.Text = "Clear All"
+        Button1.UseVisualStyleBackColor = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Button1)
+        Controls.Add(addBtn)
+        Controls.Add(newWordInput)
+        Controls.Add(OutputBox)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents OutputBox As TextBox
+    Friend WithEvents newWordInput As TextBox
+    Friend WithEvents addBtn As Button
+    Friend WithEvents Button1 As Button
+End Class
---
+++ text-logger/Form1.vb
@@ -0,0 +1,47 @@
+﻿
+
+Imports System.Diagnostics.Tracing
+
+Public Class Form1
+    Dim words As New List(Of String)()
+
+    Private Class Console
+        Public Sub log()
+            Dim currentDateTime As DateTime = DateTime.Now
+
+            clear()
+            For i As Integer = 0 To Form1.words.Count - 1
+                Form1.OutputBox.AppendText(Form1.words(i) & " ---" & currentDateTime & Environment.NewLine)
+            Next i
+        End Sub
+
+        Public Sub clear()
+            Form1.OutputBox.Clear()
+        End Sub
+    End Class
+
+
+    Private Sub addBtn_Click(sender As Object, e As EventArgs) Handles addBtn.Click
+        Dim newWord As String = newWordInput.Text
+        Dim console As Console = New Console
+
+        If newWord = "" Then
+            MessageBox.Show("Please enter word inside field")
+        Else
+            words.Add(newWord)
+            console.log()
+            newWordInput.Text = ""
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim result As DialogResult = MessageBox.Show("Are you sure you want to clear?", "Confirmation", MessageBoxButtons.OKCancel, MessageBoxIcon.Question)
+        Dim console As Console = New Console
+
+        If result = DialogResult.OK Then
+            words.Clear()
+            console.clear()
+        End If
+
+    End Sub
+End Class
